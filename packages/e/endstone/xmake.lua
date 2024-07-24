1b3738de55ed6e4f0d5f946b13662fb682586a1d
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-07-24 17:31:11 +0800

    Update xmake.lua

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -1,10 +1,9 @@
 package("endstone")
    set_urls("https://github.com/EndstoneMC/endstone.git")
 
-    on_install(function (package)
-        local configs = {}
-        if package:config("shared") then
-            configs.kind = "shared"
-        end
-        import("package.tools.xmake").install(package, configs)
+    on_install("windows", function (package)
+        os.exec("build.bat");
+        os.cp("lib/include/bedrock", package:installdir("include"))
+        os.cp("lib/include/endstone", package:installdir("include"))
+        os.cp("lib/endstone_runtime.lib", package:installdir("lib"))
     end)

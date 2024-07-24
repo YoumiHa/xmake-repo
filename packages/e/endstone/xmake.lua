1a7be77e0fdeab2492c092ec0a2a7df66f3c13ce
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-07-24 14:44:04 +0800

    Update xmake.lua

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -2,6 +2,4 @@ package("endstone")
 
     set_urls("https://github.com/EndstoneMC/endstone.git")
 
-    on_install(function (package)
-        import("package.tools.xmake").install(package)
-    end)
+   

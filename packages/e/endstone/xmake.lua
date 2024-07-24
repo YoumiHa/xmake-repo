4ae373be7b6f9ac55fe1ecbf1e4b9bf075a10941
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-07-24 18:04:17 +0800

    Update xmake.lua

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -1,6 +1,6 @@
 package("endstone")
    add_urls("https://raw.githubusercontent.com/YoumiHa/endstone/main/endstone.tar")
-   add_versions("latest","29302ba52aadcbcec0e19a671dca963d50a3c33a75434f80700bfef7d96a030f")
+   add_versions("latest","6ce746789b6defcaf027e476f97a6800013b0b090f3758383f67fbbe0640d483")
     on_install("windows", function (package)
         os.exec("build.bat");
         os.cp("lib/include/bedrock", package:installdir("include"))
